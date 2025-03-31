9f364125ab010c410d4e17eb4e4512f29a816afc
YashPatil321 <94503166+YashPatil321@users.noreply.github.com>
2025-03-30 21:03:22 -0700

    env files created

---
+++ lib/dbConnect.ts
@@ -0,0 +1,33 @@
+// lib/dbConnect.ts
+import mongoose from "mongoose";
+
+const MONGODB_URI: string = process.env.MONGODB_URI!;
+
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable in .env.local"
+  );
+}
+
+// Use globalThis for a cross-platform global variable.
+// TypeScript doesn't know about this, so we cast to 'any'.
+let cached = (globalThis as any).mongoose;
+
+if (!cached) {
+  cached = (globalThis as any).mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      bufferCommands: false,
+    }).then((mongoose) => mongoose);
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default dbConnect;
